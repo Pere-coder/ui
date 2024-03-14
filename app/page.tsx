dfde63b4a0ddb18352b249346b8c487b55ffac8c
Pere-coder <godspowerpere@yahoo.com>
2024-03-14 21:47:53 +0100

    ui added

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,10 @@
 import Image from "next/image";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-
+import Navbar from "@/components/Navbar/page"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center bg-black text-white">
-      <div className="flex items-center gap-2 mt-10">
-        <h1 className="text-white hover:cursor-pointer">My Avatar</h1>
-        <Avatar className="hover:cursor-pointer">
-          <AvatarImage src="https://github.com/shadcn.png" />
-        <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-      </div>
+    <main className="flex flex-col items-center">
+      <Navbar/>
     </main>
   );
 }
---
+++ components/Navbar/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import * as React from "react";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+
+export default function NavigationMenuDemo() {
+  return (
+    <NavigationMenu className="flex gap-10 mt-5">
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink>Link</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+      <div className="flex items-center gap-2">
+        <h1 className="hover:cursor-pointer">My Avatar</h1>
+        <Avatar className="hover:cursor-pointer">
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
+    </NavigationMenu>
+  );
+}
